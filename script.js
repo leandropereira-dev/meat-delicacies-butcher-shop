1248225528fdc15c76cffae552782f010ddc16ad
Leandro <leandropereira.dev@gmail.com>
2023-12-02 11:49:23 -0300

    50% completo da sessão contato

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {
     const menu = document.querySelector(".coluna-menu");
-    if (window.scrollY > 150) {
+    if (window.scrollY > 146) {
         menu.classList.add("fixed");
     } else {
         menu.classList.remove("fixed");
