2bd455f56b2783f6610f95278a03b6352a6ad886
Leandro <leandropereira.dev@gmail.com>
2023-12-04 19:33:20 -0300

    adiciona gradiente no header e sessão quem somos

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ window.onscroll = function() {
         menu.classList.remove("fixed");
     }
 };
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.coluna-menu a');
+
+    function highlightMenu() {
+      let index = sections.length;
+
+      while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+      navLinks.forEach((link) => link.classList.remove('ativo'));
+      navLinks[index].classList.add('ativo');
+    }
+
+    highlightMenu(); // Initial call to highlight menu based on current position
+
+    document.addEventListener('scroll', highlightMenu);
+  });
