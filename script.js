2e17cba918a88a301db7c28a6b458460f75e7fb3
Leandro <leandropereira.dev@gmail.com>
2023-12-05 23:12:26 -0300

    altera cor dos icones e ativa o menu conforme a sessão

--- script.js
+++ script.js
@@ -7,21 +7,28 @@ window.onscroll = function() {
     }
 };
 
-
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('.coluna-menu a');
 
     function highlightMenu() {
-      let index = sections.length;
+    let index = sections.length;
 
-      while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
 
-      navLinks.forEach((link) => link.classList.remove('ativo'));
-      navLinks[index].classList.add('ativo');
+    navLinks.forEach((link) => link.classList.remove('ativo'));
+
+    if (index >= 0 && index < navLinks.length) {
+        navLinks[index].classList.add('ativo');
+    } else if (window.scrollY === 0) {
+        // Se estiver no topo da página, manter ativo na navegação da "home"
+        navLinks[0].classList.add('ativo');
+    } else {
+        // Se chegou ao topo da última seção, manter ativo na última navegação
+        navLinks[navLinks.length - 1].classList.add('ativo');
     }
-
-    highlightMenu(); // Initial call to highlight menu based on current position
+}
+    highlightMenu(); 
 
     document.addEventListener('scroll', highlightMenu);
-  });
+});
