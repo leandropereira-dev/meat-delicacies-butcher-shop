666dbe031f3d31188af6f8212a6a2ac3053eb184
Leandro <leandropereira.dev@gmail.com>
2023-12-18 21:10:49 -0300

    cocluída a etapa desktop. Falta a responsividade.

--- script.js
+++ script.js
@@ -33,91 +33,91 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  const todosOsMeiosCheckbox = document.getElementById('todosOsMeios');
-  const smsCheckbox = document.getElementById('smsCheckbox');
-  const emailCheckbox = document.getElementById('emailCheckbox');
+    const todosOsMeiosCheckbox = document.getElementById('todosOsMeios');
+    const smsCheckbox = document.getElementById('smsCheckbox');
+    const emailCheckbox = document.getElementById('emailCheckbox');
 
-  todosOsMeiosCheckbox.addEventListener('click', marcarTodosOsMeios);
-  smsCheckbox.addEventListener('click', marcarIndividualmente);
-  emailCheckbox.addEventListener('click', marcarIndividualmente);
+    todosOsMeiosCheckbox.addEventListener('click', marcarTodosOsMeios);
+    smsCheckbox.addEventListener('click', marcarIndividualmente);
+    emailCheckbox.addEventListener('click', marcarIndividualmente);
 
-  function marcarTodosOsMeios() {
-      const meioNotificacaoCheckboxes = document.querySelectorAll('.meioNotificacao');
+function marcarTodosOsMeios() {
+    const meioNotificacaoCheckboxes = document.querySelectorAll('.meioNotificacao');
 
-      meioNotificacaoCheckboxes.forEach((checkbox) => {
-          checkbox.checked = todosOsMeiosCheckbox.checked;
-      });
-  }
+    meioNotificacaoCheckboxes.forEach((checkbox) => {
+        checkbox.checked = todosOsMeiosCheckbox.checked;
+    });
+}
 
-  function marcarIndividualmente() {
-      todosOsMeiosCheckbox.checked = smsCheckbox.checked && emailCheckbox.checked;
-  }
+function marcarIndividualmente() {
+    todosOsMeiosCheckbox.checked = smsCheckbox.checked && emailCheckbox.checked;
+}
 
-  function validarFormulario() {
-      resetErrors();
+function validarFormulario() {
+    resetErrors();
 
-      const nome = document.getElementById('nome').value;
-      if (nome.trim() === '' || nome.split(' ').length < 2) {
-          displayError('nomeError', 'Nome não pode estar em branco e deve possuir pelo menos 2 nomes');
-          return false;
-      }
+    const nome = document.getElementById('nome').value;
+    if (nome.trim() === '' || nome.split(' ').length < 2) {
+        displayError('nomeError', 'Nome não pode estar em branco e deve possuir pelo menos 2 nomes');
+        return false;
+    }
 
-      const email = document.getElementById('email').value;
-      if (!validateEmail(email)) {
-          displayError('emailError', 'E-mail inválido');
-          return false;
-      }
+    const email = document.getElementById('email').value;
+    if (!validateEmail(email)) {
+        displayError('emailError', 'E-mail inválido');
+        return false;
+    }
 
-      const telefone = document.getElementById('telefone').value.replace(/[\s()-]/g, '');
-      if (telefone.length !== 11) {
-          displayError('telefoneError', 'Telefone deve ter 11 caracteres');
-          return false;
-      }
+    const telefone = document.getElementById('telefone').value.replace(/[\s()-]/g, '');
+    if (telefone.length !== 11) {
+        displayError('telefoneError', 'Telefone deve ter 11 caracteres');
+        return false;
+    }
 
-      const preferencias = document.querySelectorAll('input[name="preferencias"]:checked');
-      if (preferencias.length === 0) {
-          displayError('preferenciasError', 'Ao menos uma preferência deve ser marcada');
-          return false;
-      }
+    const preferencias = document.querySelectorAll('input[name="preferencias"]:checked');
+    if (preferencias.length === 0) {
+        displayError('preferenciasError', 'Ao menos uma preferência deve ser marcada');
+        return false;
+    }
 
-      const carnesFavoritas = document.getElementById('favoritos');
-      if (carnesFavoritas.value === 'selecione') {
-          displayError('favoritosError', 'Selecione um tipo de carne favorita');
-          return false;
-      }
+    const carnesFavoritas = document.getElementById('favoritos');
+    if (carnesFavoritas.value === 'selecione') {
+        displayError('favoritosError', 'Selecione um tipo de carne favorita');
+        return false;
+    }
 
-      const mensagem = document.getElementById('mensagem').value;
-      if (mensagem.trim().length < 5) {
-          displayError('mensagemError', 'Mensagem deve ter pelo menos 5 caracteres');
-          return false;
-      }
+    const mensagem = document.getElementById('mensagem').value;
+    if (mensagem.trim().length < 5) {
+        displayError('mensagemError', 'Mensagem deve ter pelo menos 5 caracteres');
+        return false;
+    }
 
-      if (!todosOsMeiosCheckbox.checked && !smsCheckbox.checked && !emailCheckbox.checked) {
-          displayError('notificacaoError', 'Selecione pelo menos uma opção de notificação');
-          return false;
-      }
+    if (!todosOsMeiosCheckbox.checked && !smsCheckbox.checked && !emailCheckbox.checked) {
+        displayError('notificacaoError', 'Selecione pelo menos uma opção de notificação');
+        return false;
+    }
 
-      alert('Formulário enviado com sucesso!');
-      return true;
-  }
+    alert('Formulário enviado com sucesso!');
+    return true;
+}
 
-  function validateEmail(email) {
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return regex.test(email);
-  }
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
 
-  function displayError(elementId, message) {
-      const errorElement = document.getElementById(elementId);
-      errorElement.textContent = message;
-  }
+function displayError(elementId, message) {
+    const errorElement = document.getElementById(elementId);
+    errorElement.textContent = message;
+}
 
-  function resetErrors() {
-      const errorElements = document.querySelectorAll('.error');
-      errorElements.forEach((element) => {
-          element.textContent = '';
-      });
-  }
+function resetErrors() {
+    const errorElements = document.querySelectorAll('.error');
+    errorElements.forEach((element) => {
+        element.textContent = '';
+    });
+}
 
-  const enviarBotao = document.getElementById('enviar');
-  enviarBotao.addEventListener('click', validarFormulario);
+    const enviarBotao = document.getElementById('enviar');
+    enviarBotao.addEventListener('click', validarFormulario);
 });
